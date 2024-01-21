a8387ab39d5ab1087b0916078a103f499c6678cc
Christina Carvalho <ccarvalho@oakland.edu>
2024-01-20 21:45:20 -0500

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+## How to use the app
+This app supports conversions between the following units: centimeters (cm), meters (m), feet (ft), and inches (in).
+
+The user first inputs their initial value.
+Then the user inputs the abbreviated units of that initial value.
+Finally, the user inputs the abbreviated units for what they want to convert that initial value to.
+The converted value is calculated and shown.
+
+If the user wants to exit the app, they can type -1 when the program asks for the initial value.
+If the user enters an unsupported unit, the program will tell the user so they can try again.
+
+## Example
+A user wants to convert 1 centimeter to meters.
+
+Input:
+1
+cm
+m
+
+Output:
+Conversion from 1 cm to m is 0.01 m
---
+++ src/App.java
@@ -0,0 +1,107 @@
+import java.util.Scanner;
+
+public class App {
+    static void weight(){
+
+    }
+    public static void main(String[] args) throws Exception {
+        int run = 1;
+        do{
+            double num1 = 0;
+            String unit1 = "", unit2 = "";
+            Scanner input = new Scanner(System.in);
+            System.out.println("Welcome to Metric Converter! Enter -1 to exit.");
+            System.out.println("Supported conversions: cm, m, in, ft.");
+            System.out.println("Enter value: ");
+            num1 = input.nextDouble();
+            if(num1 == -1){
+                System.exit(0);
+            }
+
+            System.out.println("Enter abreviated units of value: ");
+            input.nextLine();
+            unit1 = input.nextLine();
+
+            System.out.println("Enter abreviated units of value to convert to: ");
+            unit2 = input.nextLine();
+
+            double num2 = 0;
+            switch(unit1){
+                case "cm":
+                    switch(unit2){
+                        case "m": 
+                            num2 = num1/100;  
+                            break;
+                        case "in": 
+                            num2 = num1/2.54;
+                            break;
+                        case "ft": 
+                            num2 = num1/30.48;
+                            break;
+                        default:
+                            System.out.println("Cannot convert to "+unit2+". Try with a different unit.");
+                            break;
+                    }
+                    break;
+
+                case "m":
+                    switch(unit2){
+                        case "cm": 
+                            num2 = num1*100;
+                            break;
+                        case "in": 
+                            num2 = num1*39.37;
+                            break;
+                        case "ft": 
+                            num2 = num1*3.2808399;
+                            break;
+                        default:
+                            System.out.println("Cannot convert to "+unit2+". Try with a different unit.");
+                            break;
+                    }
+                    break;
+
+                case "in":
+                    switch(unit2){
+                        case "m": 
+                            num2 = num1/39.37;
+                            break;
+                        case "cm": 
+                            num2 = num1*2.54;
+                            break;
+                        case "ft": 
+                            num2 = num1/12;
+                            break;
+                        default:
+                            System.out.println("Cannot convert to "+unit2+". Try with a different unit.");
+                            break;
+                    }
+                    break;
+
+                case "ft":
+                    switch(unit2){
+                        case "m": 
+                            num2 = num1/3.2808399;
+                            break;
+                        case "in": 
+                            num2 = num1*12;
+                            break;
+                        case "cm": 
+                            num2 = num1*30.48;
+                            break;
+                        default:
+                            System.out.println("Cannot convert to "+unit2+". Try with a different unit.");
+                            break;
+                    }
+                    break;
+
+                default:
+                    System.out.println(unit1+" is not a vaild conversion. Try with a different unit.");
+                    break;
+            }
+            System.out.println("Conversion from "+num1+" "+unit1+" to "+unit2+" is "+num2+" "+unit2);
+            System.out.println("**************************");
+
+        } while (run ==1);
+    }
+}
